3652b6392d7471a20f5c7ea764ac58071aeba912
Mohamed Hassan <mhass405@gmail.com>
2021-04-28 21:36:47 +0000

    Add data for database

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO users (name, email, password)
+VALUES ('Rihanna', 'riri@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Doja Cat', 'riri@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Beyonce', 'bey@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 3, 'canada', 'dremond st', 'vancouver', 'BC', 'H1L 4C8', 'YES'),
+(2, 'Blank corner ', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 9512, 6, 1, 10, 'canada', 'heyo st', 'ottawa', 'ON', 'K4T 4C8', 'YES'),
+(3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 1111, 2, 3, 5, 'US', 'Murica st', 'NYc', 'NY', 'A1A 4G8', 'YES');
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date)
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (1, 1, 1, 5, 'message'),
+(2, 2, 2, 4, 'message'),
+(3, 3, 3, 3, 'message');
